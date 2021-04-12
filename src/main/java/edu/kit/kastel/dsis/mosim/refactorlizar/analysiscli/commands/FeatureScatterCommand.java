0850ecb5b21e14dcdda39c1633ae6f562bac7b84
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-04-12 23:21:12 +0200

    add level feature

--- src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/FeatureScatterCommand.java
+++ src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/FeatureScatterCommand.java
@@ -1,59 +1,65 @@
 package edu.kit.kastel.dsis.mosim.refactorlizar.analysiscli.commands;
 
-import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.featurescatter.FeatureScatterAnalyzer;
-import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.featurescatter.PackageVisitor;
-import edu.kit.kastel.sdq.case4lang.refactorlizar.model.Feature;
-import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
-import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import spoon.Launcher;
-import spoon.reflect.CtModel;
-import spoon.reflect.declaration.CtPackage;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api.SearchLevels;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.featurescatter.FeatureScatterAnalyzer;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.core.LanguageParser;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.core.SimulatorParser;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
+import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 @ShellComponent
 @ShellCommandGroup("bad-smell-analysis")
 public class FeatureScatterCommand {
 
+    private static final String ERROR_MESSAGE = "Cannot Analyze language path: %s code path: %s";
+    private static final String STARTING_FEATURE_SCATTERING_ANALYSIS = "Starting Feature Scattering Analysis";
     private static final Logger logger = LogManager.getLogger(FeatureScatterAnalyzer.class);
 
-    @ShellMethod("Find occurrences of the feature scattering smell")
-    public void findFeatureScatteringSmell(String language, String code){
-        logger.info("Starting Feature Scattering Analysis");
-        Launcher languageLauncher = new Launcher();
-        languageLauncher.addInputResource(language);
-        CtModel languageModel = languageLauncher.buildModel();
-
-        Collection<Feature> feature = new ArrayList<>();
-        languageModel.getAllPackages().stream().filter(v->isFeaturePackage(v.getQualifiedName())).forEach(t -> feature.add(new Feature(t, null)));
-        ModularLanguage lang = new ModularLanguage(feature);
-
-        Launcher simulatorLauncher = new Launcher();
-        simulatorLauncher.addInputResource(code);
-        CtModel spoonSimulatorModel = simulatorLauncher.buildModel();
-
-        SimulatorModel simulatorModel = new SimulatorModel(spoonSimulatorModel.getAllPackages().stream().map(v -> new Feature(v,null)).collect(Collectors.toList()));
-
-        FeatureScatterAnalyzer fsa = new FeatureScatterAnalyzer();
-        fsa.init(lang, simulatorModel);
-        logger.info(fsa.getDescription());
-        if(fsa.supportsFullAnalysis()){
-            logger.info(fsa.fullAnalysis());
+    @ShellMethod("Find occurrences of the feature scattering smell on type level")
+    public void findFeatureScatteringSmellType(String language, String code) {
+        logger.info(STARTING_FEATURE_SCATTERING_ANALYSIS);
+        FeatureScatterAnalyzer fsa = createFeatureScatterAnalyzer(language, code);
+        if (fsa.supportsFullAnalysis()) {
+            logger.info(fsa.fullAnalysis(SearchLevels.TYPE));
         } else {
-            logger.error("Cannot Analyze "+language+" and "+code);
+            logger.error(ERROR_MESSAGE, language, code);
         }
     }
 
-    private boolean isFeaturePackage(String qn){
-        if(qn.contains(".impl")|qn.contains(".util")|qn.isEmpty())
-            return false;
-        return true;
+    @ShellMethod("Find occurrences of the feature scattering smell on package level")
+    public void findFeatureScatteringSmellPackage(String language, String code) {
+        logger.info(STARTING_FEATURE_SCATTERING_ANALYSIS);
+        FeatureScatterAnalyzer fsa = createFeatureScatterAnalyzer(language, code);
+        logger.info(fsa.getDescription());
+        if (fsa.supportsFullAnalysis()) {
+            logger.info(fsa.fullAnalysis(SearchLevels.PACKAGE));
+        } else {
+            logger.error(ERROR_MESSAGE, language, code);
+
+        }
+    }
+    @ShellMethod("Find occurrences of the feature scattering smell on component level")
+    public void findFeatureScatteringSmellComponent(String language, String code) {
+        logger.info(STARTING_FEATURE_SCATTERING_ANALYSIS);
+        FeatureScatterAnalyzer fsa = createFeatureScatterAnalyzer(language, code);
+        logger.info(fsa.getDescription());
+        if (fsa.supportsFullAnalysis()) {
+            logger.info(fsa.fullAnalysis(SearchLevels.COMPONENT));
+        } else {
+            logger.error(ERROR_MESSAGE, language, code);
+        }
+    }
+    private FeatureScatterAnalyzer createFeatureScatterAnalyzer(String language, String code) {
+        SimulatorModel model = new SimulatorModel(new SimulatorParser().parseLanguage(code));
+        ModularLanguage lang = new ModularLanguage(new LanguageParser().parseLanguage(language));
+        FeatureScatterAnalyzer fsa = new FeatureScatterAnalyzer();
+        fsa.init(lang, model);
+        return fsa;
     }
 }
