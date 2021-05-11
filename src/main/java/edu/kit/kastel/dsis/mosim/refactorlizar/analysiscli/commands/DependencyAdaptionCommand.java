73b68c3b6ff7de6be320f6b54af30e39c3114c75
Sandro <layornos@gmail.com>
2021-05-11 19:28:36 +0200

    changes according to pr#5

--- src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/DependencyAdaptionCommand.java
+++ src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/DependencyAdaptionCommand.java
@@ -6,7 +6,7 @@ import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,46 +25,29 @@ public class DependencyAdaptionCommand {
 
     @ShellMethod("Adapt dependencies from modular language to monolithic program")
     public void adaptDependencies(String program, String csv){
-        StringBuilder sb = new StringBuilder();
-        // Load CSV
         Map<String, String> dependencyMapping = new HashMap<>();
         try {
-            String line;
-            BufferedReader csvReader = new BufferedReader(new FileReader(csv));
-            while((line = csvReader.readLine()) != null){
+            // Load CSV
+            Path csvPath = Paths.get(csv);
+            Files.lines(csvPath).forEach(line -> {
                 String[] split = line.split(DELIMITER);
                 if(split.length == 2)
                     dependencyMapping.put("import " + split[0] +";", "import " + split[1] + ";");
+            });
+
+           // Change Dependencies according to CSV
+            Stream<Path> filesInPath = Files.walk(Paths.get(program));
+            List<Path> javaFiles = filesInPath.filter(f -> f.toString().endsWith(".java")).collect(Collectors.toList());
+            for(Path path: javaFiles){
+                List<String> replaced = new ArrayList<>();
+                Files.lines(path).forEach(line -> {
+                    String replacementLine = dependencyMapping.getOrDefault(line,line);
+                    replaced.add(replacementLine);
+                });
+                Files.write(path, replaced);
             }
-        } catch (IOException e){
-            logger.error(e.getStackTrace());
-        }
-        //
-        try (Stream<Path> walk = Files.walk(Paths.get(program))) {
-
-            List<String> result = walk.map(x -> x.toString())
-                    .filter(f -> f.endsWith(".java")).collect(Collectors.toList());
-
-            for(String javaFile : result){
-                if(javaFile.contains("RDSeffSwitch")) {
-                    Path path = Paths.get(javaFile);
-                    Stream<String> lines = Files.lines(path);
-                    List<String> replaced = new ArrayList<>();
-                    lines.forEach(line -> {
-                        if(dependencyMapping.get(line) != null){
-                            replaced.add(line.replace(line, dependencyMapping.get(line)));
-                        }
-                        else{
-                            replaced.add(line);
-                        }
-                    });
-                    Files.write(path, replaced);
-                    lines.close();
-                }
-            }
-
         } catch (IOException e) {
-            logger.error(e.getStackTrace());
+            logger.error(e);
         }
     }
 }
