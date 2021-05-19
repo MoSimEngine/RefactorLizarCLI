247cb666d2fc1a91b37f3a2264f068d7ecf9b835
Sandro <layornos@gmail.com>
2021-05-19 09:31:25 +0200

    apply formatter

--- src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/DependencyDirectionCommand.java
+++ src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/DependencyDirectionCommand.java
@@ -1,11 +1,5 @@
 package edu.kit.kastel.dsis.mosim.refactorlizar.analysiscli.commands;
 
-import java.lang.invoke.MethodHandles;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.shell.standard.ShellCommandGroup;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api.Report;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.dependencydirection.DependencyDirectionAnalyzer;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.commons.Settings;
@@ -13,40 +7,50 @@ import edu.kit.kastel.sdq.case4lang.refactorlizar.core.LanguageParser;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.core.SimulatorParser;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
+import java.lang.invoke.MethodHandles;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
 @ShellComponent
 @ShellCommandGroup("bad-smell-analysis")
 public class DependencyDirectionCommand {
-  private static final String STARTING_DEPENDENCY_CYCLE_ANALYSIS = "Starting dependency direction Analysis";
-  private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+    private static final String STARTING_DEPENDENCY_CYCLE_ANALYSIS =
+            "Starting dependency direction Analysis";
+    private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
-  @ShellMethod("Find occurrences of the dependency direction smells on type level. Layers must be ordered from bottom to top and separated by ','")
-  public void findDependencyDirectionSmellType(String language, String code, String layer) {
-      logger.info(STARTING_DEPENDENCY_CYCLE_ANALYSIS);
-      logger.info("{}", () -> createReport(language, code, "type", layer));
-  }
+    @ShellMethod(
+            "Find occurrences of the dependency direction smells on type level. Layers must be ordered from bottom to top and separated by ','")
+    public void findDependencyDirectionSmellType(String language, String code, String layer) {
+        logger.info(STARTING_DEPENDENCY_CYCLE_ANALYSIS);
+        logger.info("{}", () -> createReport(language, code, "type", layer));
+    }
 
-  @ShellMethod("Find occurrences of the dependency direction smells on package level. Layers must be ordered from bottom to top and separated by ','")
-  public void findDependencyDirectionSmellPackage(String language, String code, String layer) {
-      logger.info(STARTING_DEPENDENCY_CYCLE_ANALYSIS);
-      logger.info("{}", () -> createReport(language, code, "package", layer));
+    @ShellMethod(
+            "Find occurrences of the dependency direction smells on package level. Layers must be ordered from bottom to top and separated by ','")
+    public void findDependencyDirectionSmellPackage(String language, String code, String layer) {
+        logger.info(STARTING_DEPENDENCY_CYCLE_ANALYSIS);
+        logger.info("{}", () -> createReport(language, code, "package", layer));
+    }
 
-  }
-  @ShellMethod("Find occurrences of the dependency direction smells on component level. Layers must be ordered from bottom to top and separated by ','")
-  public void findDependencyDirectionSmellComponent(String language, String code, String layer) {
-      logger.info(STARTING_DEPENDENCY_CYCLE_ANALYSIS);
-      logger.info("{}", () -> createReport(language, code, "component",layer));
+    @ShellMethod(
+            "Find occurrences of the dependency direction smells on component level. Layers must be ordered from bottom to top and separated by ','")
+    public void findDependencyDirectionSmellComponent(String language, String code, String layer) {
+        logger.info(STARTING_DEPENDENCY_CYCLE_ANALYSIS);
+        logger.info("{}", () -> createReport(language, code, "component", layer));
+    }
 
-  }
+    private Report createReport(String language, String code, String level, String layer) {
+        SimulatorModel model = new SimulatorModel(SimulatorParser.parseSimulator(code));
+        ModularLanguage lang = new ModularLanguage(LanguageParser.parseLanguage(language));
 
-  private Report createReport(String language, String code, String level, String  layer) {
-      SimulatorModel model = new SimulatorModel(SimulatorParser.parseSimulator(code));
-      ModularLanguage lang = new ModularLanguage(LanguageParser.parseLanguage(language));
-
-      DependencyDirectionAnalyzer dda = new DependencyDirectionAnalyzer();
-      logger.info(dda.getDescription());
-      Settings settings  = dda.getSettings();
-      settings.setValue("level", level);
-      settings.setValue("layers", layer);
-      return dda.analyze(lang, model, settings);
-  }
+        DependencyDirectionAnalyzer dda = new DependencyDirectionAnalyzer();
+        logger.info(dda.getDescription());
+        Settings settings = dda.getSettings();
+        settings.setValue("level", level);
+        settings.setValue("layers", layer);
+        return dda.analyze(lang, model, settings);
+    }
 }
--- src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/FeatureScatterCommand.java
+++ src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/FeatureScatterCommand.java
@@ -1,11 +1,5 @@
 package edu.kit.kastel.dsis.mosim.refactorlizar.analysiscli.commands;
 
-import java.lang.invoke.MethodHandles;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.shell.standard.ShellCommandGroup;
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.api.Report;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.analyzer.featurescatter.FeatureScatterAnalyzer;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.commons.Settings;
@@ -13,13 +7,19 @@ import edu.kit.kastel.sdq.case4lang.refactorlizar.core.LanguageParser;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.core.SimulatorParser;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.ModularLanguage;
 import edu.kit.kastel.sdq.case4lang.refactorlizar.model.SimulatorModel;
-
+import java.lang.invoke.MethodHandles;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.shell.standard.ShellCommandGroup;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
 
 @ShellComponent
 @ShellCommandGroup("bad-smell-analysis")
 public class FeatureScatterCommand {
 
-    private static final String STARTING_FEATURE_SCATTERING_ANALYSIS = "Starting Feature Scattering Analysis";
+    private static final String STARTING_FEATURE_SCATTERING_ANALYSIS =
+            "Starting Feature Scattering Analysis";
     private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
     @ShellMethod("Find occurrences of the feature scattering smell on type level")
@@ -33,13 +33,12 @@ public class FeatureScatterCommand {
         logger.info(STARTING_FEATURE_SCATTERING_ANALYSIS);
         logger.info("{}", () -> createReport(language, code, "package"));
     }
+
     @ShellMethod("Find occurrences of the feature scattering smell on component level")
     public void findFeatureScatteringSmellComponent(String language, String code) {
         logger.info(STARTING_FEATURE_SCATTERING_ANALYSIS);
         logger.info("{}", () -> createReport(language, code, "component"));
-
     }
-
 
     private Report createReport(String language, String code, String level) {
         SimulatorModel model = new SimulatorModel(SimulatorParser.parseSimulator(code));
@@ -47,7 +46,7 @@ public class FeatureScatterCommand {
 
         FeatureScatterAnalyzer fca = new FeatureScatterAnalyzer();
         logger.info(fca.getDescription());
-        Settings settings  = fca.getSettings();
+        Settings settings = fca.getSettings();
         settings.setValue("level", level);
         return fca.analyze(lang, model, settings);
     }
