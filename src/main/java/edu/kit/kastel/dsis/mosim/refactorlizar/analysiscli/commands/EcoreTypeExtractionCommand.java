517ce73f23e416e7c535793a61276f94993c419d
Sandro <layornos@gmail.com>
2021-05-25 18:25:24 +0200

    changes as requested

--- src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/EcoreTypeExtractionCommand.java
+++ src/main/java/edu/kit/kastel/dsis/mosim/refactorlizar/analysiscli/commands/EcoreTypeExtractionCommand.java
@@ -3,20 +3,15 @@ package edu.kit.kastel.dsis.mosim.refactorlizar.analysiscli.commands;
 import java.io.File;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.*;
+import java.util.*;
 import java.util.stream.Stream;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
+import static org.apache.logging.log4j.util.Strings.LINE_SEPARATOR;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
@@ -30,45 +25,42 @@ import org.xml.sax.SAXException;
 @ShellCommandGroup("Language Knowledge Extraction")
 public class EcoreTypeExtractionCommand {
     private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().lookupClass());
-    private final String ECORE_FILE_ENDING = ".ecore";
+    private final String ECORE_FILE_ENDING = "ecore";
+    private final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**."+ECORE_FILE_ENDING);
     private final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
     @ShellMethod("Metamodels Type Extraction")
     public void showTypesInMetamodels(String rootPath) {
         Map<String, List<String>> metamodels = new HashMap<>();
         try (Stream<Path> paths = Files.walk(Paths.get(rootPath))) {
-            paths.filter(path -> path.toString().endsWith(ECORE_FILE_ENDING))
+            paths.filter(matcher::matches)
                     .forEach(
                             path -> {
                                 readMetamodels(metamodels, path);
-                                printMetamodelTypes(metamodels);
                             });
         } catch (IOException e) {
             logger.error(e);
         }
+        printMetamodelTypes(metamodels);
     }
 
     private void printMetamodelTypes(Map<String, List<String>> metamodels) {
-        StringBuilder sb = new StringBuilder();
-        metamodels.forEach(
-                (key, value) -> {
-                    sb.append(Strings.LINE_SEPARATOR)
-                            .append("Metamodel:")
-                            .append(Strings.LINE_SEPARATOR)
-                            .append(key)
-                            .append(Strings.LINE_SEPARATOR)
-                            .append("Types:")
-                            .append(Strings.LINE_SEPARATOR);
-                    value.forEach(type -> sb.append(type).append(Strings.LINE_SEPARATOR));
-                });
-        logger.info(sb);
+        StringJoiner joiner = new StringJoiner(LINE_SEPARATOR,LINE_SEPARATOR,LINE_SEPARATOR);
+        for(var entry : metamodels.entrySet()) {
+            joiner.add("Metamodel:")
+                    .add(entry.getKey())
+                    .add("Types:");
+            entry.getValue().forEach(joiner::add);
+            joiner.add(LINE_SEPARATOR);
+        }
+        logger.info(joiner);
     }
 
     private void readMetamodels(Map<String, List<String>> metamodels, Path path) {
         metamodels.put(path.toString(), new ArrayList<>());
         try {
             DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
-            Document ecoreFile = documentBuilder.parse(new File(String.valueOf(path)));
+            Document ecoreFile = documentBuilder.parse(path.toFile());
             ecoreFile.getDocumentElement().normalize();
             NodeList types = ecoreFile.getElementsByTagName("eClassifiers");
             for (int i = 0; i < types.getLength(); i++) {
